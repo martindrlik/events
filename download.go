7766b1a6c06d943c89916df2c88eae23ba0fffac
Martin Drlík <martindrlik@hotmail.com>
2018-02-11 16:30:06 +0100

    events: from, to form value instead of json IDs

--- download.go
+++ download.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/martindrlik/events/store/local"
 )
 
 func downloadHandler(w http.ResponseWriter, r *http.Request) {
-	type Download struct {
-		IDs []int64
-	}
-	defer r.Body.Close()
-	decoder := json.NewDecoder(r.Body)
-	downloadRequest := Download{}
-	err := decoder.Decode(&downloadRequest)
+	from, err := strconv.Atoi(r.FormValue("from"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	err = local.Zip(downloadRequest.IDs, w)
+	to, err := strconv.Atoi(r.FormValue("to"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	ids := make([]int64, 0, 1+(to-from))
+	for i := from; i <= to; i++ {
+		ids = append(ids, int64(i))
+	}
+	err = local.Zip(ids, w)
 	if err != nil {
 		log.Println(err)
 		http.Error(w, "creating zip to download failed", http.StatusInternalServerError)
